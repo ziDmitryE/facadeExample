c5ec0bcd170191a8a9c2c19a404d001bf3788c50
Dmitriy Eremenko <er_inbox@mail.ru>
2022-05-26 11:26:34 +0300

    first commit

---
+++ src/main/java/BinOps.java
@@ -0,0 +1,14 @@
+public class BinOps {
+
+    public String sum(String a, String b) {
+
+        return Integer.toBinaryString((Integer.parseInt(a, 2) + Integer.parseInt(b, 2)));
+
+    }
+
+    public String mult(String a, String b) {
+
+        return Integer.toBinaryString((Integer.parseInt(a, 2) * Integer.parseInt(b, 2)));
+
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        BinOps bins = new BinOps();
+
+        System.out.println(bins.sum("110010", "10"));
+
+        System.out.println(bins.mult("110010", "10"));
+    }
+}
